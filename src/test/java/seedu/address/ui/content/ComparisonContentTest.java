49ab4e6b6e65a251762a7474b60b86eec9310d07
aqfw <116161343+aqfw@users.noreply.github.com>
2026-03-26 21:17:35 +0800

    Fix formatting errors

--- src/test/java/seedu/address/ui/content/ComparisonContentTest.java
+++ src/test/java/seedu/address/ui/content/ComparisonContentTest.java
@@ -1,8 +1,6 @@
 package seedu.address.ui.content;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
