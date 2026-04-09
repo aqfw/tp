222bac46f4f15d16507eb2774d85ce836a79e56a
rjustyn1 <142522694+rjustyn1@users.noreply.github.com>
2026-04-09 19:48:31 +0800

    Improve error messages for Command outlet delete

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -16,9 +16,13 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-    public static final String MESSAGE_INVALID_OUTLET_DISPLAYED_INDEX = "The outlet index provided is invalid";
-    public static final String MESSAGE_INVALID_TAG_COMBO_DISPLAYED_INDEX = "The tag combo index provided is invalid";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is out of range. "
+            + "Use list to view valid person indices.";
+    public static final String MESSAGE_INVALID_OUTLET_DISPLAYED_INDEX = "The outlet index provided is out of range. "
+            + "Use outlet list to view valid outlet indices.";
+    public static final String MESSAGE_INVALID_TAG_COMBO_DISPLAYED_INDEX =
+            "The tag combo index provided is out of range. "
+            + "Use listtagcombo to view valid tag combo indices.";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
